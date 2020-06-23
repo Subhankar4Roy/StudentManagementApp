88bd1a9e3c9b5178543b0203564aa305d06fda87
Subhankar Roy <subhankarroylive@gmail.com>
2020-06-23 23:29:01 +0530

    initial setup for frontEnd & backEnd

---
+++ backEnd/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`))
