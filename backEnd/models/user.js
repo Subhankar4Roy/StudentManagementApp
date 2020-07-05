5ad60e6dccbe0715e93e454cd7592847c2864bc2
Subhankar Roy <subhankarroylive@gmail.com>
2020-07-05 22:11:56 +0530

    bcrypt add

--- backEnd/models/user.js
+++ backEnd/models/user.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
+    name:{type:String,
+        required:true},
     email:{type:String,
         required:true},
     password: {type:String,
-        required:true}
+        required:true},
+    created: { type:Date,
+        default: Date.now
+
+    }
 })
 module.exports = mongoose.model('user', userSchema)
