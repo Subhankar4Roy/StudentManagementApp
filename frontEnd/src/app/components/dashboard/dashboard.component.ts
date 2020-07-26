43efb8b7452bc1927fbacba38b3924a7a3ab6e42
Subhankar Roy <subhankarroylive@gmail.com>
2020-07-26 14:37:07 +0530

    chart works

--- frontEnd/src/app/components/dashboard/dashboard.component.ts
+++ frontEnd/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentsService } from '../../services/students.service';
+import * as Highcharts from 'highcharts';
+
 
 
 @Component({
@@ -8,15 +10,99 @@ import { StudentsService } from '../../services/students.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  students = []
+  total;
+  students =[];
+  chartOptions = {};
+  Highcharts = Highcharts;
   constructor(private studentsService : StudentsService) { }
-
+  
+  getTotalStudents(){
+    this.studentsService.getStudents().subscribe(
+      // res => this.students =res,
+      res =>{console.log(res.length) 
+      this.total = res.length} ,
+      err => console.log(err),
+    )
+     //this.total = this.students.length;
+  }
   ngOnInit(): void {
-    this.studentsService.getStudents()
-        .subscribe(
-          res => this.students =res,
-          err => console.log(err)
-        )
+    this.chartOptions = {
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'Total Students in each Departments'
+      },
+      accessibility: {
+          announceNewData: {
+              enabled: true
+          }
+      },
+      xAxis: {
+          type: 'category'
+      },
+      yAxis: {
+          title: {
+              text: 'Total students Department-Wise'
+          }
+  
+      },
+      legend: {
+          enabled: false
+      },
+      plotOptions: {
+          series: {
+              borderWidth: 0,
+              dataLabels: {
+                  enabled: true,
+                  format: '{point.y}'
+              }
+          }
+      },
+  
+      tooltip: {
+          headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+          pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b><br/>'
+      },
+  
+      series: [
+          {
+              name: "Department",
+              colorByPoint: true,
+              data: [
+                  {
+                      name: "CSE",
+                      y: this.total,
+                  },
+                  {
+                      name: "IT",
+                      y: 10,
+                  },
+                  {
+                      name: "EE",
+                      y: 7,
+                  },
+                  {
+                      name: "ME",
+                      y: 5,
+                  },
+                  {
+                      name: "ECE",
+                      y: 4,
+                  },
+                  {
+                      name: "MCA",
+                      y: 1,
+                  }
+              ]
+          }
+      ]
+  };
+
+  this.total = this.getTotalStudents()
+  
   }
 
+
+  
 }
